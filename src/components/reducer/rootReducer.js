682ba3b5f60c05edd31df8ca793e43d109974663
MrGautamojha <47115026+MrGautamojha@users.noreply.github.com>
2020-02-19 14:37:07 +0530

    Update rootReducer.js

--- src/components/reducer/rootReducer.js
+++ src/components/reducer/rootReducer.js
@@ -9,6 +9,7 @@ function rootReducer(state=initialState,action){
         state.post[action.payload[0]]=action.payload[1]
         console.log(state.post)
         return {post:state.post}
+            
         case 'DELETE_POST':
         var data=action.payload[0]
         let rslt=new Object()
